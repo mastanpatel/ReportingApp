1feb0710ff2c30c8bf62651720552605c6e76e20
mastan patel <mastanpatel@19932021.onmicrosoft.com>
2023-09-09 14:43:39 +0530

    added the grid and ribbon to main view

---
+++ ReportingApp.DAL/Repository/CustomerRepository.cs
@@ -0,0 +1,23 @@
+﻿using ReportingApp.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportingApp.DAL.Repository
+{
+    public class CustomerRepository
+    {
+        WideWorldImportersContext _context;
+        public CustomerRepository()
+        {
+            _context = new WideWorldImportersContext();
+        }
+
+        public List<Customer> getCustomers()
+        {
+            return _context.Customers.Take(20).ToList();
+        }
+    }
+}
---
+++ ReportingApp/VIewModel/VIewModel.cs
@@ -0,0 +1,39 @@
+﻿using ReportingApp.DAL.Repository;
+using DevExpress.Mvvm;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System;
+using ReportingApp.DAL.Models;
+using DevExpress.Mvvm.Xpf;
+using DevExpress.Mvvm.Native;
+
+namespace ReportingApp.VIewModel
+{
+    class VIewModel : ViewModelBase
+    {
+        CustomerRepository repo;
+
+        public VIewModel()
+        {
+            if(DesignerProperties.GetIsInDesignMode(new DependencyObject()))
+            {
+                Customers = new ObservableCollection<Customer>();
+            }
+            else
+            {
+                repo = new CustomerRepository();
+                Customers = new ObservableCollection<Customer>(repo.getCustomers());
+            }
+            
+
+        }
+
+        public ObservableCollection<Customer> Customers
+        {
+            get => GetValue<ObservableCollection<Customer>>();
+            private set => SetValue(value);
+
+        }
+    }
+}
